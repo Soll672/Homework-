3481cead511f6ae9228a02110bffc6b507d385af
Soll672 <stalker.gtx990@gmail.com>
2024-02-08 20:04:46 +0600

    first commit01

--- Abdulaziz_39-1_hw1.py
+++ Abdulaziz_39-1_hw1.py
@@ -1,6 +1,4 @@
-class superhero:
-    people = 'people'
-
+class Superhero:
     def __init__(self, name, nickname, superpower, health_points, catchphrase):
         self.name = name
         self.nickname = nickname
@@ -11,16 +9,59 @@ class superhero:
     def double_health(self):
         self.health_points *= 2
 
-    def __str__(self):
-        return f"nickname: {self.nickname}, superpower: {self.superpower}, health: {self.health_points}"
+    def print_phrase(self):
+        print(self.catchphrase)
 
-    def __len__(self):
-        return len(self.catchphrase)
+class AirHero(Superhero):
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, fly=False, damage=0):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.fly = fly
+        self.damage = damage
 
+    def double_health(self):
+        super().double_health()
+        self.fly = True
 
-super_hero = superhero("john", "gauss prime", "super speed", 185, "I am named after Karl Friedrich Gauss!")
+    def print_phrase(self):
+        print("true in the true_phrase")
 
-super_hero.double_health()
-print(super_hero)
+class EarthHero(Superhero):
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, fly=False, damage=0):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.fly = fly
+        self.damage = damage
 
-print(len(super_hero))
+    def double_health(self):
+        super().double_health()
+        self.fly = True
+
+    def print_phrase(self):
+        print("true in the true_phrase")
+
+class Villain(AirHero):
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, fly=False, damage=0):
+        super().__init__(name, nickname, superpower, health_points, catchphrase, fly, damage)
+        self.people = 'monster'
+
+    def gen_x(self):
+        pass
+
+    def crit(self):
+        self.damage *= 2
+
+air_hero = AirHero("Tony Stark", "Iron man", "Flight", 500, "You idiot, wake up, you're going to be a firefighter today!")
+earth_hero = EarthHero("Bob", "Rock", "Earth Manipulation", 400, "Earth is my strength!")
+villain = Villain("Victor von Doom", "Dr.Doom", "Time manipulation", 800, "Fear my wrath!")
+
+air_hero.double_health()
+earth_hero.double_health()
+villain.double_health()
+
+air_hero.print_phrase()
+earth_hero.print_phrase()
+
+villain.crit()
+
+print(air_hero.__dict__)
+print(earth_hero.__dict__)
+print(villain.__dict__)
